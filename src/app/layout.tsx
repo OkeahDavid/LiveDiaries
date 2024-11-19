09d951a85ba03f2fff7847ef49cb28f177f7f1be
David <okeahdavid@gmail.com>
2024-11-19 16:40:01 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 
 export const metadata: Metadata = {
-  title: 'LiveDiaries',
+  title: 'Live Diaries',
   description: 'Welcome to the LiveDiaries Website',
 }
 
